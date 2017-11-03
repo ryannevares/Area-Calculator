7c9dbfc790f9213c0a3c979743d6d4739d0b474d
Ryan Nevares <ryannevares@gmail.com>
2017-11-03 00:09:45 -0700

    Version 2.6 release: implemented slow-typing throughout and added QUIT to main menu

--- Area_Calculator_FINAL.py
+++ Area_Calculator_FINAL.py
@@ -1,10 +1,12 @@
 """
 Author: Ryan Nevares
-Version 2.5
-Date: 10/17/2017
+Version 2.6
+Date: 11/2/2017
 Area Calculator
 This program will calculate the areas of various shapes.  First we will prompt the user for which shape they want to use, then ask for the measurements we need to calculate the area of the shape.  This is a complete rewrite of one of my earliest Python programs and attempts to do the exact same thing it did with ~100 fewer lines and fewer bugs :)
 Enjoy!
+
+Please report any bugs or suggestions to ryannevares@gmail.com
 """
 # start by importing the commands we will need from other libraries
 from math import pi, sqrt
@@ -18,7 +20,7 @@ def print_slow(string):
     for letter in fill(string):
         stdout.write(letter)
         stdout.flush()
-        sleep(.09)
+        sleep(.03)
 
 # Print initial message to the user
 print ""
@@ -27,18 +29,21 @@ sleep(.5)
 print "\nThe current time and date are: %s/%s/%s %s:%s \n" % (now.month, now.day, now.year, now.hour, now.minute)
 sleep(1)
 
-################# FUNCTION DEFINITIONS #################################
 # This function will give the user a list of options
 def printoptions():
     print "Options:"
     sleep(.5)
-    option_list = ["C for circle", "T for triangle", "R for rectangle", "S for square", "P for parallelogram", "TD for trapazoid", "E for ellipse", "SR for sphere", "CN for cone", "CY for cylinder", "CB for cube"]
+    option_list = ["C for circle", "T for triangle", "R for rectangle", "S for square", "P for parallelogram", "TD for trapazoid", "E for ellipse", "SR for sphere", "CN for cone", "CY for cylinder", "CB for cube", "Q to QUIT"]
     for i in option_list:
-        print i
+        print_slow (i)
+        print ""
         sleep(.1)
+
 def print_hint(): # This is repeated several times, so we define it only once
     sleep(0.2)
-    print "Don't forget to check your units!\n"
+    print_slow ("Don't forget to check your units!")
+    print "\n\n"
+
 # Define functions for each possible shape
 # Inputs must be in the form of floats, we will make sure they are floats later
 def circle(radius):
@@ -87,12 +92,13 @@ def cube(sidelength):
 
 # The last 3 lines of all the last 11 functions are the same, lets change that
 def return_area(area):
-    print "Calculating...\n"
-    sleep(2)
+    print_slow ("Calculating...")
+    print ""
+    sleep(1.5)
     return area
 
 # Define a function to get user's input
-def getInput():
+def get_input():
     print ""
     option = raw_input("Enter shape here: ")
     option = option.upper()
@@ -102,83 +108,111 @@ def getInput():
 def again():
     playagain = raw_input("Perform another calculation? (y/n): ")
     if playagain == "y" or playagain == "Y":
-        print "Alright!  Let's go! \n "
+        print_slow ("Alright!  Let's go!")
+        print "\n\n\n"
         sleep(1.5)
         calculate()
     elif playagain == "n" or playagain == "N":
-        print " \nSorry to see you go, exiting now ...."
+        print_slow ("Sorry to see you go.")
+        print "\n\n\n\n"
+        print_slow ("exiting now ....")
+        print "\n\n"
         sleep(.4)
         exit()
     else:
-        print "Please choose y or n: \n"
+        print_slow ("Please choose y or n")
+        print "\n\n"
         sleep (1)
         return again()
 
 # Deals with bad values for shape dimensions
 def bad_value():
-    print "ERROR: Invalid values entered! \n"
+    print_slow ("ERROR: Invalid values entered!")
+    print ""
     return calculate()
 # Finally the main function!
 def calculate():
     printoptions()
-    option = getInput()
+    option = get_input()
+
     """ For each possible option, make sure the input can be converted to a float.
     If it can, pass the float to the corresponding function.
     If not, call the calculate function.
     """
+
     if option == "C":
-        print "You have selected CIRCLE"
+        print_slow ("You have selected CIRCLE")
+        print ""
         sleep(.4)
         radius = raw_input("Enter the radius: ")
         if radius.isdigit():
             radius = float(radius)
-            print "\nThe area of the circle is %.2f" % circle(radius)
+            print ""
+            print_slow ("The area of the circle is %.2f" % circle(radius))
+            print ""
             print_hint()
+            sleep(.5)
         else: bad_value()
     elif option == "T":
-        print "You have selected TRIANGLE"
+        print_slow ("You have selected TRIANGLE")
+        print ""
         sleep (.4)
         base = raw_input("Enter the length of the base: ")
         height = raw_input("Enter the height: ")
         if base.isdigit() and height.isdigit():
             base = float(base)
             height = float(height)
-            print "\nThe area of the triangle is %.2f" % triangle(base, height)
+            print ""
+            print_slow ("The area of the triangle is %.2f" % triangle(base, height))
+            print ""
             print_hint()
+            sleep(.5)
         else: bad_value()
     elif option == "R":
-        print "You have selected RECTANGLE"
+        print_slow ("You have selected RECTANGLE")
+        print ""
         sleep(.4)
         length = raw_input("Enter the length: ")
         width = raw_input("Enter the width: ")
         if length.isdigit() and width.isdigit():
             length = float(length)
             width = float(width)
-            print "\nThe area of the rectangle is %.2f" % rectangle(length, width)
+            print ""
+            print_slow ("The area of the rectangle is %.2f" % rectangle(length, width))
+            print ""
             print_hint()
+            sleep(.5)
         else: bad_value()
     elif option == "S":
-        print "You have selected SQUARE"
-        sleep(.4)
+        print_slow ("You have selected SQUARE")
+        print ""
+        sleep(.5)
         side = raw_input("Enter the length of one side: ")
         if side.isdigit():
             side = float(side)
-            print "\nThe area of the square is %.2f" % square(side)
+            print ""
+            print_slow ("The area of the square is %.2f" % square(side))
+            print ""
             print_hint()
         else: bad_value()
     elif option == "P":
-        print "You have selected PARALLELOGRAM"
+        print_slow ("You have selected PARALLELOGRAM")
+        print ""
         sleep(.4)
         base = raw_input("Enter the base: ")
         height = raw_input("Enter the height: ")
         if base.isdigit() and height.isdigit():
             base = float(base)
             height = float(height)
-            print "\nThe area of the parallelogram is %.2f" % para(base,height)
+            print ""
+            print_slow ("The area of the parallelogram is %.2f" % para(base,height))
+            print ""
             print_hint()
+            sleep(.5)
         else: bad_value()
     elif option == "TD":
-        print "You have selected TRAPAZOID"
+        print_slow ("You have selected TRAPAZOID")
+        print ""
         sleep(.4)
         top = raw_input("Enter the length of the top: ")
         bottom = raw_input("Enter the length of the bottom: ")
@@ -187,60 +221,90 @@ def calculate():
             top = float(top)
             bottom = float(bottom)
             height = float(height)
-            print "\nThe area of the trapazoid is %.2f" % trap(top,bottom,height)
+            print ""
+            print_slow ("The area of the trapazoid is %.2f" % trap(top,bottom,height))
+            print ""
             print_hint()
+            sleep(.5)
         else: bad_value()
     elif option == "E":
-        print "You have selected ELLIPSE"
+        print_slow ("You have selected ELLIPSE")
+        print ""
         sleep(.4)
         longrad = raw_input("Enter the radius on the long axis: ")
         shortrad = raw_input("Enter the radius on the short axis: ")
         if longrad.isdigit() and shortrad.isdigit():
             longrad = float(longrad)
             shortrad = float(shortrad)
-            print "\nThe area of the ellipse is %.2f" % ellipse(longrad,shortrad)
+            print ""
+            print_slow ("The area of the ellipse is %.2f" % ellipse(longrad,shortrad))
+            print ""
             print_hint()
+            sleep(.5)
         else: bad_value()
     elif option == "SR":
-        print "You have selected SPHERE"
+        print_slow ("You have selected SPHERE")
+        print ""
         sleep(.4)
         radius = raw_input("Enter the radius: ")
         if radius.isdigit():
             radius = float(radius)
-            print "\nThe surface area of the sphere is %.2f" % sphere(radius)
+            print ""
+            print_slow ("The surface area of the sphere is %.2f" % sphere(radius))
+            print ""
             print_hint()
+            sleep(.5)
         else: bad_value()
     elif option == "CN":
-        print "You have selected CONE"
+        print_slow ("You have selected CONE")
+        print ""
         sleep(.4)
         radius = raw_input("Enter the radius: ")
         height = raw_input("Enter the height: ")
         if radius.isdigit() and height.isdigit():
             radius = float(radius)
             height = float(height)
-            print "\nThe surface area of the cone is %.2f" % cone(radius,height)
+            print ""
+            print_slow ("The surface area of the cone is %.2f" % cone(radius,height))
+            print ""
             print_hint()
+            sleep(.5)
         else: bad_value()
     elif option == "CY":
-        print "You have selected CYLINDER"
+        print_slow ("You have selected CYLINDER")
+        print ""
         sleep(.4)
         radius = raw_input("Enter the radius: ")
         height = raw_input("Enter the height: ")
         if radius.isdigit() and height.isdigit():
             radius = float(radius)
             height = float(height)
-            print "\nThe surface area of the cylinder is %.2f" % cylinder(radius,height)
+            print ""
+            print_slow ("The surface area of the cylinder is %.2f" % cylinder(radius,height))
+            print ""
             print_hint()
+            sleep(.5)
         else: bad_value()
     elif option == "CB":
-        print "You have selected CUBE"
+        print_slow ("You have selected CUBE")
+        print ""
         sleep(.4)
         side = raw_input("Enter the length of one side: ")
         if side.isdigit():
             side = float(side)
-            print "\nThe surface area of the cube is %.2f" % cube(side)
+            print ""
+            print_slow ("The surface area of the cube is %.2f" % cube(side))
+            print ""
             print_hint()
+            sleep(.5)
         else: bad_value()
+    elif option == "Q":
+        print_slow ("Sorry to see you go.")
+        print "\n\n\n\n"
+        print_slow ("exiting now ....")
+        print "\n\n"
+        sleep(.4)
+        exit()
     else: bad_value()
     return again()
 
